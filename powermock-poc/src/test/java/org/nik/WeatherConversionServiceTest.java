fe938a85bb095542d9b263fb2cd27e1b24e73b2a
Nikhilesh Yacham <Nikhilesh Yacham@TORAHMLH3NMQ72.am.mds.honda.com>
2018-03-07 16:07:27 -0800

    Adding Test cases to mock the private method and

--- powermock-poc/src/test/java/org/nik/WeatherConversionServiceTest.java
+++ powermock-poc/src/test/java/org/nik/WeatherConversionServiceTest.java
@@ -9,12 +9,12 @@ import org.mockito.Spy;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(fullyQualifiedNames="org.nik.*")
 public class WeatherConversionServiceTest {
 	
-	private WeatherconversionService weatherCnvrsnService = new WeatherconversionService();
 	
 	@Spy
 	private WeatherconversionService spy = new WeatherconversionService();
@@ -33,8 +33,8 @@ public class WeatherConversionServiceTest {
 	@Test
 	public void testConversionMethodReturnsValidFormat(){
 		PowerMockito.when(WeatherConversionUtil.convertToFahrenheit(Mockito.anyDouble())).thenReturn(200.00);
-		Assert.assertTrue(weatherCnvrsnService.convertToFahrenheit(15.00).matches("\\d*.\\d+"+DEGREE+"[CF]"));
-		Assert.assertTrue(weatherCnvrsnService.convertToFahrenheit(15.00).matches("\\d*.\\d+"+DEGREE+"[CF]"));
+		Assert.assertTrue(spy.convertToFahrenheit(15.00).matches("\\d*.\\d+"+DEGREE+"[CF]"));
+		Assert.assertTrue(spy.convertToFahrenheit(15.00).matches("\\d*.\\d+"+DEGREE+"[CF]"));
 		PowerMockito.verifyStatic(Mockito.times(2));// should be used in the combination to the below step, 
 													// which will essentially verify if the below invocation 
 													// is done for specified number of times
@@ -47,6 +47,12 @@ public class WeatherConversionServiceTest {
         PowerMockito.when(spy, "getWeatherFormattedstring",Mockito.anyDouble(), Mockito.anyChar()).thenReturn("200.00"+DEGREE+"C");
 		String convertToFahrenheit = spy.convertToFahrenheit(200.00);
 		Assert.assertEquals("200.00"+DEGREE+"C", convertToFahrenheit);
+	}
+	
+	@Test
+	public void testInvokePrivateMethod() throws Exception{
+		String formattedString  = Whitebox.invokeMethod(spy, "getWeatherFormattedstring",Double.valueOf(200.00), 'F');
+		Assert.assertEquals("200.00"+DEGREE+"F", formattedString);
 	}
 	
 }
