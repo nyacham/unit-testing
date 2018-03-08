3612314773e3d2de42a58e8a5e2024fcfb6f4404
Nikhilesh Yacham <Nikhilesh Yacham@TORAHMLH3NMQ72.am.mds.honda.com>
2018-03-07 16:23:57 -0800

    Adding test case to mock the final method

---
+++ powermock-poc/src/main/java/org/nik/WeatherUtilName.java
@@ -0,0 +1,8 @@
+package org.nik;
+
+public class WeatherUtilName {
+
+	public final String printWeatherConversionUtilClassName(){
+		return WeatherConversionUtil.class.getName();
+	}
+}
--- powermock-poc/src/main/java/org/nik/WeatherconversionService.java
+++ powermock-poc/src/main/java/org/nik/WeatherconversionService.java
@@ -1,6 +1,11 @@
 package org.nik;
 
+import java.text.DecimalFormat;
+
 public class WeatherconversionService {
+	
+	
+	private WeatherUtilName waUtilName = new WeatherUtilName();
 
 	private static final String DEGREE = "\u00b0";
 
@@ -11,11 +16,16 @@ public class WeatherconversionService {
 	 * @return
 	 */
 	public String convertToFahrenheit(double celcius) {
-		System.out.println("Converting: "+getWeatherFormattedstring(celcius, 'C'));
+		System.out.println("Converting: " + getWeatherFormattedstring(celcius, 'C'));
 		return getWeatherFormattedstring(WeatherConversionUtil.convertToFahrenheit(celcius), 'F');
 	}
 
 	private String getWeatherFormattedstring(double celcius, char unit) {
-		return String.format("%s%s%s", celcius, DEGREE, unit);
+		return String.format("%s%s%s", new DecimalFormat("0.00").format(celcius), DEGREE, unit);
+	}
+	
+	public String message(){
+		System.out.println("Going to Invoke final Method");
+		return waUtilName.printWeatherConversionUtilClassName();
 	}
 }
